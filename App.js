a77aeae40301fb78d772019e5b0f1ff5a5ebe028
anoopjain <51991397+anoop-diaspark@users.noreply.github.com>
2023-11-14 12:34:31 +0530

    new changes

--- App.js
+++ App.js
@@ -8,7 +8,8 @@ export default class App extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      filteredEmoji: filterEmoji("", 20)
+      filteredEmoji: filterEmoji("", 20),
+	  news : newS
     };
   }
 
